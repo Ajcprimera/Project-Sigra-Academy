4c71678652ec6982aa786467e9bf9cba09fc28e5
GabrielM555 <gabrielmatasoto2005@gmail.com>
2025-12-28 14:01:20 -0400

    feat(manager.controller.mjs): se implementan funciones para obtener cursos y crear nuevos

---
+++ backend/src/modules/academic-manager-III/manager.controller.mjs
@@ -0,0 +1,17 @@
+import * as managerModel from './manager.model.mjs'
+import { validateCreateCourse } from './manager.schema.mjs'
+
+// Obtener cursos de un estudiante
+export const getCoursesByStudent = async (req, res) => {
+  
+}
+
+// Obtener detalle de un curso
+export const getCourseDetail = async (req, res) => {
+  
+}
+
+// Crear un nuevo curso
+export const createCourse = async (req, res) => {
+  
+}
